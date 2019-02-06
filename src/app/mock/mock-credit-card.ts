3473bf783ff27848584349ef2a6b1c323aaa5510
Unknown <scharbonneau5@hotmail.ca>
2019-02-06 17:25:59 -0500

    Add credit card and styling

---
+++ src/app/mock/mock-credit-card.ts
@@ -0,0 +1,7 @@
+import { CreditCard } from '../classes/credit-card';
+
+export const CREDITCARD: CreditCard = {
+    name: 'Marcel Leboeuf',
+    number: '5889 9000 2874 1999',
+    cvv: '127',
+};
--- src/app/mock/mock-spectacles.ts
+++ src/app/mock/mock-spectacles.ts
@@ -5,9 +5,4 @@ export const SPECTACLES: Spectacle[] = [
   { id: 12, name: 'Depuis longtemps', artist: 'Yoan', price: 24.5 , date: '01/02/2019', hour: 18.5, description:'Yoan fait son retour. Un spectacle émouvant.'},
   { id: 13, name: 'La vie chante', artist: 'BRUNO PELLETIER', price: 9.99 , date: '05/05/2019', hour: 19.25, description:'bbbbbrrrrrunnnoooo!'},
   { id: 14, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-  { id: 15, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-  { id: 16, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-  { id: 17, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-  { id: 18, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-  { id: 19, name: '', artist: '', price: 0 , date: '', hour: 0, description:''},
-];
+ ];
