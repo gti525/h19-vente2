0bf67a8761f039575caa3b24ab0477fd8bd5435c
Francis De Vizio <46574345+francisdevizio@users.noreply.github.com>
2019-02-08 15:06:46 -0500

    Update app-routing.module.ts
    Added the cart route; the header button now works.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CheckoutRecapComponent } from './checkout-recap/checkout-recap.component';
 import { CheckoutConfirmationComponent } from './checkout-confirmation/checkout-confirmation.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
 	{ path: "", redirectTo: "home", pathMatch: "full" },
 	{ path: "home", component: HomeComponent },
   { path: 'checkout-recap', component: CheckoutRecapComponent },
-  { path: 'checkout-confirmation', component: CheckoutConfirmationComponent }
+  { path: 'checkout-confirmation', component: CheckoutConfirmationComponent },
+  { path: "cart", component: CartComponent }
 ];
 
 @NgModule({
