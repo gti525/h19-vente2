81db3be494889a8dc6b3a0c308956d98cad327d0
Unknown <scharbonneau5@hotmail.ca>
2019-02-15 18:18:25 -0500

    update route

--- server.js
+++ server.js
@@ -4,13 +4,19 @@ const path = require('path');
  
 const app = express();
  
+var port = process.env.PORT || 8080;
+
+
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/vente2'));
- 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/vente2/index.html'));
+
+// Page Angular de présentation des messages
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/dist/vente2/index.html'));
 });
+
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+console.log('Serveur démarré sur le port ' + port);
