270437cd7cca986e3616a58ec22e30bf06b5ecde
Samuel Charbonneau <scharbonneau5@hotmail.ca>
2019-02-28 14:27:31 -0500

    add Acces-control headers allow

--- app.ts
+++ app.ts
@@ -45,7 +45,7 @@ createConnection(<ConnectionOptions>{
 
 
   router.get('/', function (req, res) {
-    res.json({ message: 'Bienvenue sur l\'API de vente2 GTI525' });
+    res.json({ message: 'Bienvenue suar l\'API de vente2 GTI525' });
   });
 
   // register all application routes
@@ -62,6 +62,26 @@ createConnection(<ConnectionOptions>{
   // FIN DES ROUTES API
   //lier router à la route /api
   app.use('/api', router);
+
+  // Add headers
+  router.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
 
   // distribue l'application Angular par défaut.
   app.use(express.static(__dirname + '/dist/vente2'));
