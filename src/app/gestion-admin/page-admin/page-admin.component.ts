65ad1a5c25475293c027f42811621f206244a946
Trésor NDENG <tresor-elie.ndeng-nkouanga.1@ens.etsmtl.ca>
2019-03-04 19:44:20 -0500

    Propriétés css des images

--- src/app/gestion-admin/page-admin/page-admin.component.ts
+++ src/app/gestion-admin/page-admin/page-admin.component.ts
@@ -3,16 +3,33 @@ import { first } from 'rxjs/operators';
 
 import { User } from '../_models';
 import { UserService } from '../_services';
+import { Event } from '../../models/event';
+import { EventService } from '../../event.service';
 
-@Component({templateUrl: './page-admin.component.html'})
+@Component({
+    selector: 'page-admin',
+    templateUrl: './page-admin.component.html',
+    styleUrls: ['./page-admin.component.css']
+})
 export class AdminComponent implements OnInit {
     users: User[] = [];
+    events: Event[] = [];
 
-    constructor(private userService: UserService) {}
+    constructor(
+        private userService: UserService,
+        private eventService: EventService
+    ) {}
 
     ngOnInit() {
         this.userService.getAll().pipe(first()).subscribe(users => { 
             this.users = users; 
         });
+        console.log(this.getSpec());
+    }
+
+    getSpec() {
+        this.eventService.getEvents().subscribe((res : Event[])=>{
+        this.events = res;     
+      });
     }
 }
