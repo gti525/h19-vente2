ac05542df4e648101588df4254ba06647731edb5
Samuel Charbonneau <scharbonneau5@hotmail.ca>
2019-03-14 10:22:24 -0400

    fix build errors and cors authorization

--- app.ts
+++ app.ts
@@ -50,7 +50,7 @@ createConnection(<ConnectionOptions>{
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, authorization');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
