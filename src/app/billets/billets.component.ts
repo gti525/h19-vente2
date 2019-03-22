50d455cbd886505e4a3a37e4826e8354ad045703
Trésor NDENG <tresor-elie.ndeng-nkouanga.1@ens.etsmtl.ca>
2019-03-22 19:47:12 -0400

    Insertion pub

--- src/app/billets/billets.component.ts
+++ src/app/billets/billets.component.ts
@@ -12,7 +12,7 @@ import { validateConfig } from '@angular/router/src/config';
 
 export class BilletsComponent implements OnInit {
     public events: Event[] = [];
-    e: any;
+    urlValue: any;
 
     constructor(
         private eventService: EventService,
@@ -25,14 +25,17 @@ export class BilletsComponent implements OnInit {
           });
     }
 
-    updateEvent(idSpectacle, imgSrc) {
-        var test = document.getElementsByTagName('urlIMG')[0];
-        alert(test);
-        //this.eventService.updateEvent(idSpectacle, imgSrc)
+    updateEvent(idSpectacle) {
+        //var test = document.getElementById('urlIMG').innerText;
+        //alert(this.urlValue);
+        this.eventService.updateEvent(idSpectacle, this.urlValue);
     }
 
-    doSomething(val)
-    {
-        alert(val.name);
+    onKey(event){ 
+        //alert("Test");
+        if (event.key === "Enter") {
+            this.urlValue = event.target.value;
+          }
+        //alert(event.target.value);
     }
 }
