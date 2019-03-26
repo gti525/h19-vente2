864e9cf26999fa73a4d27906aff3aa2021884540
Trésor NDENG <tresor-elie.ndeng-nkouanga.1@ens.etsmtl.ca>
2019-03-25 23:57:09 -0400

    Pub (show-detail)

--- src/app/show-detail/show-detail.component.ts
+++ src/app/show-detail/show-detail.component.ts
@@ -27,6 +27,7 @@ export class ShowDetailComponent implements OnInit {
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id']; 
       this.getSingleEventById(this.id);
+      this.ngafterviewinit();
       // (+) converts string 'id' to a number
    });
   }
@@ -48,5 +49,9 @@ export class ShowDetailComponent implements OnInit {
         this.router.navigate(['/cart']);
       }
     });
+  }
+
+  ngafterviewinit (){
+    document.addEventListener("DOMContentLoaded",function(){const e="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY0LCJpYXQiOjE1NTMyOTA1OTJ9.rzTQxpXHOedSfEtC3zgDkwifhTQj50gornz99vNQwAA";const t=function(){if("undefined"!=typeof Storage&&localStorage.getItem("gti525analytic")){const e=JSON.parse(localStorage.getItem("gti525analytic"));if(new Date(e.expiration).getTime()>(new Date).getTime())return e.clientId}return}();t?function(t){let n=new XMLHttpRequest;n.open("GET","https://gti525-analitycs.herokuapp.com/api/v1/banners/code",!0),n.onload=function(o){4===n.readyState&&200===n.status&&Function(`return (${n.responseText})`)()(t,e)},n.setRequestHeader("x-access-token",e),n.send()}(t):function(){let t=new XMLHttpRequest;t.open("GET","https://gti525-analitycs.herokuapp.com/api/v1/analytics/code",!0),t.onload=function(n){4===t.readyState&&200===t.status&&Function(`return (${t.responseText})`)()(e)},t.setRequestHeader("x-access-token",e),t.send()}()},!1);
   }
 }
