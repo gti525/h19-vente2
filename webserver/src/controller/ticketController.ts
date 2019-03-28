fe30670318166bbf1db570645590884cf92290ab
Francis De Vizio <francisdevizio@gmail.com>
2019-03-28 11:08:43 -0400

    Update ticketController.ts
    Get Sold tickets, get free tickets

--- webserver/src/controller/ticketController.ts
+++ webserver/src/controller/ticketController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getManager, IsNull, Any, Not } from "typeorm";
 import { Ticket } from "../entity/Ticket";
 import { Event } from "../entity/Event";
+import { async } from "@angular/core/testing";
 
 export async function getTicketsByEventId(request: Request, response: Response) {
 
@@ -32,3 +33,26 @@ export async function areTicketsSoldForEvent(event: Event): Promise<Boolean> {
 
     return areTicketsSold;
 }
+
+export async function getSoldTicketsForEvent(event: Event): Promise<Ticket[]> {
+
+    const ticketRepository = getManager().getRepository(Ticket);
+    const tickets = await ticketRepository.find({
+        event: event,
+        transaction: Not(IsNull())
+    });
+
+    return tickets;
+}
+
+export async function getFreeTicketsForEvent(event: Event): Promise<Ticket[]> {
+
+    const ticketRepository = getManager().getRepository(Ticket);
+    const tickets = await ticketRepository.find({
+        event: event,
+        transaction: IsNull()
+    });
+
+    return tickets;
+}
+
