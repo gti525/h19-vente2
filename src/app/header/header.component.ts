14c33c6cb6446f0974d9764ae8b0e60145cacab5
Simon <sim.chevrier@gmail.com>
2019-03-29 16:40:31 -0400

    Quickfix notification

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -19,7 +19,6 @@ export class HeaderComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-  	this.getCart();
   	this.headerUpdateService.notification.subscribe(data => {
   		this.getCart();
   	});
