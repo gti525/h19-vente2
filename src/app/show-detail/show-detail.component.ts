35058f467a258dd1719692dee27f22de363b8f0e
Trésor NDENG <tresor-elie.ndeng-nkouanga.1@ens.etsmtl.ca>
2019-04-05 12:48:18 -0400

    Map functionnality done with OpenStreetMap

--- src/app/show-detail/show-detail.component.ts
+++ src/app/show-detail/show-detail.component.ts
@@ -5,12 +5,19 @@ import { EventService } from '../event.service';
 import { CartService } from '../cart.service';
 import { HeaderUpdateService } from '../header-update.service';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-show-detail',
   templateUrl: './show-detail.component.html',
   styleUrls: ['./show-detail.component.css']
 })
 export class ShowDetailComponent implements OnInit {
+
+  latitude: number = 18.5204;
+  longitude: number = 73.8567;
+
+  map: any;
 
   event : Event;
   //https://angular-2-training-book.rangle.io/handout/routing/routeparams.html
@@ -26,6 +33,8 @@ export class ShowDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.initMap();
+
     this.sub = this.route.params.subscribe(params => {
       this.id = +params['id']; 
       this.getSingleEventById(this.id);
@@ -59,5 +68,53 @@ export class ShowDetailComponent implements OnInit {
 
   ngafterviewinit (){
     document.addEventListener("DOMContentLoaded",function(){const e="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY0LCJpYXQiOjE1NTMyOTA1OTJ9.rzTQxpXHOedSfEtC3zgDkwifhTQj50gornz99vNQwAA";const t=function(){if("undefined"!=typeof Storage&&localStorage.getItem("gti525analytic")){const e=JSON.parse(localStorage.getItem("gti525analytic"));if(new Date(e.expiration).getTime()>(new Date).getTime())return e.clientId}return}();t?function(t){let n=new XMLHttpRequest;n.open("GET","https://gti525-analitycs.herokuapp.com/api/v1/banners/code",!0),n.onload=function(o){4===n.readyState&&200===n.status&&Function(`return (${n.responseText})`)()(t,e)},n.setRequestHeader("x-access-token",e),n.send()}(t):function(){let t=new XMLHttpRequest;t.open("GET","https://gti525-analitycs.herokuapp.com/api/v1/analytics/code",!0),t.onload=function(n){4===t.readyState&&200===t.status&&Function(`return (${t.responseText})`)()(e)},t.setRequestHeader("x-access-token",e),t.send()}()},!1);
+  }
+
+  initMap()
+  {
+    var mousePositionControl = new ol.control.MousePosition({
+      coordinateFormat: ol.coordinate.createStringXY(4),
+      projection: 'EPSG:4326',
+      // comment the following two lines to have the mouse position
+      // be placed within the map.
+      className: 'custom-mouse-position',
+      target: document.getElementById('mouse-position'),
+      undefinedHTML: '&nbsp;'
+    });
+
+
+    this.map = new ol.Map({
+      target: 'map',
+      controls: ol.control.defaults({
+        attributionOptions: {
+          collapsible: false
+        }
+      }).extend([mousePositionControl]),
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([73.8567, 18.5204]),
+        zoom: 8
+      })
+    });
+
+    this.map.on('click', function (args) {
+      console.log(args.coordinate);
+      var lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+      console.log(lonlat);
+      
+      var lon = lonlat[0];
+      var lat = lonlat[1];
+      alert(`lat: ${lat} long: ${lon}`);
+    });
+  }
+
+  setCenter() {
+    var view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(8);
   }
 }
